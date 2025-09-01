e341d5ce32aba3ad01caaf8f952043938c6ccb5b
wolako <benowola@gmail.com>
2025-09-01 18:23:38 +0000

    v2

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -26,29 +26,30 @@
 //   return next(req);
 // };
 
-
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('auth_token');
-  const apiBase = environment.apiUrl;
 
-  // Vérifie si c'est un appel API
-  const isApiCall = req.url.startsWith(apiBase);
+  let headers: any = {};
+  let withCredentials = true; // inclut les cookies si besoin
 
-  // Ne pas ajouter le token uniquement sur les routes de login/register
+  const isApiCall = req.url.startsWith(environment.apiUrl);
   const isAuthRoute =
     req.url.includes('/auth/login') ||
     req.url.includes('/auth/admin/login') ||
     req.url.includes('/auth/register');
 
   if (isApiCall && token && !isAuthRoute) {
-    const cloned = req.clone({
-      setHeaders: { Authorization: `Bearer ${token}` }
-    });
-    return next(cloned);
+    headers['Authorization'] = `Bearer ${token}`;
   }
 
-  return next(req);
+  const cloned = req.clone({
+    setHeaders: headers,
+    withCredentials: withCredentials
+  });
+
+  return next(cloned);
 };
+
